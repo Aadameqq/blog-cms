3b1c5d6a72c3f0f20a3238758c63771f80c13b08
Aadameqq <adamek5599.sluzbowy@gmail.com>
2023-03-10 13:14:29 +0100

    fix: prettier

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import dotenv from "dotenv";
-import morgan from "morgan";
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
 // @ts-ignore
-import swaggerUI from "swagger-ui-express";
-import { decorateWithRouters } from "./routers/decorateWithRouters";
-import swaggerDocument from "swagger/swagger.json";
-import { prismaClient } from "../database/prismaClient";
+import swaggerUI from 'swagger-ui-express';
+import { decorateWithRouters } from './routers/decorateWithRouters';
+import swaggerDocument from 'swagger/swagger.json';
+import { prismaClient } from '../database/prismaClient';
 
 dotenv.config();
 
@@ -13,13 +13,13 @@ const PORT = process.env.PORT || 5500;
 
 const app = express();
 
-app.use(morgan("tiny"));
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use(morgan('tiny'));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const decoratedApp = decorateWithRouters(app);
 
 decoratedApp.listen(PORT, async () => {
-  const records = await prismaClient.test.create({ data: { name: "test" } });
+  const records = await prismaClient.test.create({ data: { name: 'test' } });
   console.log(records);
   console.log(`Listening on port ${PORT}`);
 });
